5c95ccb303209ac685a804242fdf862da2eeeb02
Philip Pretorius <philippretorius.79@gmail.com>
2023-04-13 13:47:32 +0200

    Toggle display of form added

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-let myLibrary = ['a', 'b', 'c'];
+/* let myLibrary = ['a', 'b', 'c'];
 
 function Book() {
   // the constructor...
@@ -15,5 +15,15 @@ function bookLoop() {
     // add book info to page
     // create a div or table
     // add text content to div or table
+  }
+}; */
+
+// Dropdown onclick visibility
+function toggleFunction() {
+  const targetDiv = document.getElementById("form-section");
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
   }
 }
