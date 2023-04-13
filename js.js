8c1d16cbcf2566be0760241f476b84cf4ee464cc
Philip Pretorius <philippretorius.79@gmail.com>
2023-04-13 23:36:30 +0200

    Burned dead wood, added comments, some other minor changes - still work to be done!

--- js.js
+++ js.js
@@ -1,24 +1,3 @@
-let myLibrary = ['a', 'b', 'c'];
-
-firstBookLoop();
-
-/* // Book Constructor
-function Book(title, author, pages) {
-  // the constructor...
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.info = function() {
-		return(`${title} by ${author}, ${pages} pages`)
-	}
-}
- */
-
-
-// Alternative and easier constructor
-function Book(title) {
-  this.title = title
-}
 
 // Dropdown onclick visibility
 function toggleFunction() {
@@ -30,24 +9,53 @@ function toggleFunction() {
   }
 }
 
+
+// Library Array
+let myLibrary = ['a', 'b', 'c'];
+
+// function that loops over array items
+function firstBookLoop() {
+  myLibrary.forEach((item) => {
+    const createDiv = document.createElement("div");
+    const list = document.getElementById("list");
+    createDiv.innerText = item;
+    list.append(createDiv);
+  })
+};
+
+// Call firstBookLoop
+firstBookLoop();
+
+
+// Book Constructor
+function Book2(title, author, pages) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.info = function() {
+		return(`${title} by ${author}, ${pages} pages`)
+	}
+}
+
+
+// Alternative and easier constructor
+function Book(title) {
+  this.title = title
+}
+
+
 // link input to title
 function addToArray() {
   let inputTitle = document.getElementById("title").value;
-  myLibrary.push(inputTitle); 
+  myLibrary.push(inputTitle);
+  // instead of pushing to array push to new object 
+  // push each individual property (title, author, pages)
+  // then push object with all properties to array
   console.log(inputTitle); 
   console.log(myLibrary);
 }
 
-// function that loops over array items
-function firstBookLoop() {
-    myLibrary.forEach((item) => {
-      const createDiv = document.createElement("div");
-      const list = document.getElementById("list");
-      createDiv.innerText = item;
-      list.append(createDiv);
-    })
-};
-
+// Function that loops over array an adds div
 function bookLoop() {
   const createDiv = document.createElement("div");
   const list = document.getElementById("list");
@@ -58,22 +66,30 @@ function bookLoop() {
 };
 
 
-// Event listener
+// Call addToArray
 const addBtn = document.getElementsByClassName("add-btn");
-addBtn[0].addEventListener("click", addToArray, bookLoop, function(event) {
+addBtn[0].addEventListener("click", addToArray, function(event) {
   event.preventDefault()
 });
 
+// Call bookLoop
 const addListBtn = document.getElementsByClassName("add-btn");
 addBtn[0].addEventListener("click", bookLoop, function(event) {
   event.preventDefault()
 });
 
+// Reset form
 function clearForm() {
   document.getElementById("form").reset();
 }
 
+// Call clearForm
 const addResetBtn = document.getElementsByClassName("add-btn");
 addBtn[0].addEventListener("click", clearForm, function(event) {
   event.preventDefault()
 });
+
+
+////////////// Experimental area
+
+myLibrary.prototype.push()
