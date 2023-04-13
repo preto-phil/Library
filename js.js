3ac226fb988488e8dc97af198739515d655b1bc1
Philip Pretorius <philippretorius.79@gmail.com>
2023-04-13 18:58:43 +0200

    Some JS progress - added functionality to Add Book btn

--- js.js
+++ js.js
@@ -1,8 +1,13 @@
 /* let myLibrary = ['a', 'b', 'c'];
 
-function Book() {
+function Book(title, author, pages) {
   // the constructor...
-
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.info = function() {
+		return(`${title} by ${author}, ${pages} pages`)
+	}
 }
 
 function addBookToLibrary() {
@@ -15,10 +20,11 @@ function bookLoop() {
     // add book info to page
     // create a div or table
     // add text content to div or table
+
   }
 }; */
 
-/* // Dropdown onclick visibility
+// Dropdown onclick visibility
 function toggleFunction() {
   const targetDiv = document.getElementById("form-section");
   if (targetDiv.style.display === "none") {
@@ -26,4 +32,24 @@ function toggleFunction() {
   } else {
     targetDiv.style.display = "none";
   }
-} */
+}
+
+
+// Add items to list
+function addBook() {
+  const newLi = document.createElement("li");
+  const list = document.getElementById("list");
+  newLi.innerText = "Book Stuff";
+  list.append(newLi);
+}
+
+// Event listener
+const addBtn = document.getElementsByClassName("add-btn");
+addBtn[0].addEventListener("click", addBook);
+
+
+// Prevent Default
+function addBtnClick(event) {
+  document.getElementsByClassName("add-btn");
+  event.preventDefault();
+}
